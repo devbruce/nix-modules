a9f13ee6ebb60ad4b2139432cd2817a9a7557a7d
devbruce <bruce93k@gmail.com>
2025-01-24 21:56:20 +0900

    build(nix-darwin): update system.nix

--- nix-darwin/modules/system.nix
+++ nix-darwin/modules/system.nix
@@ -1,5 +1,20 @@
 { ... }: {
   system.defaults = {
+    # dock
     dock.autohide = true;
+    dock.largesize = 32;
+
+    # finder
+    finder.AppleShowAllExtensions = true;
+    finder.AppleShowAllFiles = true;
+    finder.FXPreferredViewStyle = "clmv";
+
+    # controlcenter
+    controlcenter.BatteryShowPercentage = true;
+    controlcenter.Bluetooth = true;
+
+    # menuExtraClock
+    menuExtraClock.Show24Hour = false;
+    menuExtraClock.ShowAMPM = true;
   };
 }
